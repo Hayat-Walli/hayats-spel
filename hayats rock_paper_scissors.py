1e7656f2de9829a6360769044079eae6ade566d2
Hayat-Walli <146996491+Hayat-Walli@users.noreply.github.com>
2023-10-04 22:15:14 +0200

    Update hayats rock_paper_scissors.py

--- hayats rock_paper_scissors.py
+++ hayats rock_paper_scissors.py
@@ -6,22 +6,22 @@ STEN = 1
 SAX = 2
 PASE = 3
 
-# Definierar en funktion för att spela en omgång
+# Definierar en funktion för att spela en omgang
 def spela_omgang():
     # Begär spelarens val
-    val = input("Sten, sax eller påse? ")
+    val = input("Sten, sax eller pase? ")
 
     # Genererar datorns val
-    dator_val = random.choice([STEN, SAX, PÅSE])
+    dator_val = random.choice([STEN, SAX, PASE])
 
     # Beräknar resultatet
     if val == dator_val:
         print("Oavgjort!")
     elif val == STEN and dator_val == SAX:
         print("Du vann!")
-    elif val == SAX and dator_val == PÅSE:
+    elif val == SAX and dator_val == PASE:
         print("Du vann!")
-    elif val == PÅSE and dator_val == STEN:
+    elif val == PASE and dator_val == STEN:
         print("Du vann!")
     else:
         print("Datorn vann!")
